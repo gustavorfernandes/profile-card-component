263afc55aa4e5b5c88cec89558711337790cf983
Gustavo <gustavo.ribeiro.fernandes1@gmail.com>
2022-06-02 18:40:04 -0300

    Building the project

---
+++ src/pages/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './App.module.scss';
+
+export default function App() {
+  return (
+    <div className={style.AppStyle}>
+      <p>
+        Hello world!
+      </p>
+    </div>
+  );
+}
